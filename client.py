0a9754c8d8f26599418c0ce9cac0c64787d5bcdc
Hunter H <huntrar@gmail.com>
2020-01-22 19:41:30 -0800

    Partial non-package implementation of Clubhouse Python API

---
+++ client.py
@@ -0,0 +1,220 @@
+import os
+import json
+import requests
+from typing import List
+
+from testtype import (
+    Category,
+    CategoryType,
+    CategoryCreate,
+    CategoryUpdate,
+    Milestone,
+    EntityTemplate,
+    EntityTemplateCreate,
+    EntityTemplateUpdate,
+    StoryContentsCreate,
+)
+
+
+class ClubhouseClient:
+    def __init__(
+        self,
+        token: str,
+        baseURL: str = "https://api.clubhouse.io",
+        version: str = "v3",
+        debug: bool = False,
+    ) -> None:
+        self.token = token
+        self.baseURL = baseURL
+        self.version = version
+        self.debug = debug
+        self.apiURL = baseURL.rstrip("/") + "/api/" + version
+
+    ############
+    # Requests #
+    ############
+    def get(self, endpoint: str, params: dict = None) -> requests.Response:
+        if params is None:
+            params = {}
+        params["token"] = self.token
+        headers = {"Content-Type": "application/json"}
+        if self.debug:
+            print(
+                "curl -X GET -H \"Content-Type: application/json\" '{}/{}?{}'".format(
+                    self.apiURL,
+                    endpoint.lstrip("/"),
+                    "&".join("{}={}".format(k, v) for k, v in params.items()),
+                )
+            )
+        r = requests.get(
+            "{}/{}".format(self.apiURL, endpoint.lstrip("/")),
+            params=params,
+            headers=headers,
+        )
+        r.raise_for_status()
+        return r
+
+    def put(self, endpoint: str, data: dict = None) -> requests.Response:
+        if data is None:
+            data = {}
+        headers = {"Content-Type": "application/json"}
+        if self.debug:
+            print(
+                "curl -X PUT -H \"Content-Type: application/json\" '{}/{}?token={}' --data '{}'".format(
+                    self.apiURL, endpoint.lstrip("/"), self.token, json.dumps(data)
+                )
+            )
+        r = requests.put(
+            "{}/{}?token={}".format(self.apiURL, endpoint.lstrip("/"), self.token),
+            data=json.dumps(data),
+            headers=headers,
+        )
+        r.raise_for_status()
+        return r
+
+    def post(self, endpoint: str, data: dict = None) -> requests.Response:
+        if data is None:
+            data = {}
+        headers = {"Content-Type": "application/json"}
+        if self.debug:
+            print(
+                "curl -X POST -H \"Content-Type: application/json\" '{}/{}?token={}' --data '{}'".format(
+                    self.apiURL, endpoint.lstrip("/"), self.token, json.dumps(data)
+                )
+            )
+        r = requests.post(
+            "{}/{}?token={}".format(self.apiURL, endpoint.lstrip("/"), self.token),
+            data=json.dumps(data),
+            headers=headers,
+        )
+        r.raise_for_status()
+        return r
+
+    def delete(self, endpoint: str) -> requests.Response:
+        headers = {"Content-Type": "application/json"}
+        if self.debug:
+            print(
+                "curl -X DELETE -H \"Content-Type: application/json\" '{}/{}?token={}'".format(
+                    self.apiURL, endpoint.lstrip("/"), self.token
+                )
+            )
+        r = requests.delete(
+            "{}/{}?token={}".format(self.apiURL, endpoint.lstrip("/"), self.token),
+            headers=headers,
+        )
+        r.raise_for_status()
+        return r
+
+    ##############
+    # Categories #
+    ##############
+    def listCategories(self) -> List[Category]:
+        """List Categories returns a list of all Categories and their attributes."""
+        return [Category(j) for j in self.get("categories").json()]
+
+    def createCategory(self, category: CategoryCreate) -> Category:
+        """Create Category allows you to create a new Category in Clubhouse."""
+        return Category(self.post("categories", category).json())
+
+    def getCategory(self, category_public_id: int) -> Category:
+        """Get Category returns information about the selected Category."""
+        return Category(self.get("categories/" + str(category_public_id)).json())
+
+    def updateCategory(
+        self, category_public_id: int, category: CategoryUpdate
+    ) -> Category:
+        """Update Category allows you to replace a Category name with another name.
+        If you try to name a Category something that already exists, you will receive a 422 response."""
+        return Category(
+            self.put("categories/" + str(category_public_id), category).json()
+        )
+
+    def deleteCategory(self, category_public_id: int) -> requests.Response:
+        """Delete Category can be used to delete any Category."""
+        return self.delete("categories/" + str(category_public_id))
+
+    def listCategoryMilestones(self, category_public_id: int) -> List[Milestone]:
+        """List Category Milestones returns a list of all Milestones with the Category."""
+        return [
+            Milestone(j)
+            for j in self.get(
+                "categories/" + str(category_public_id) + "/milestones"
+            ).json()
+        ]
+
+    ####################
+    # Entity-Templates #
+    ####################
+    def listEntityTemplates(self) -> List[EntityTemplate]:
+        """List all the entity templates for an organization."""
+        return [EntityTemplate(j) for j in self.get("entity-templates").json()]
+
+    def createEntityTemplate(
+        self, entityTemplate: EntityTemplateCreate
+    ) -> EntityTemplate:
+        """Create a new entity template for your organization."""
+        return EntityTemplate(self.post("entity-templates", entityTemplate).json())
+
+    def disableStoryTemplates(self) -> requests.Response:
+        """Disables the Story Template feature for the given Organization."""
+        return self.put("entity-templates/disable")
+
+    def enableStoryTemplates(self) -> requests.Response:
+        """Enables the Story Template feature for the given Organization."""
+        return self.put("entity-templates/enable")
+
+    def getEntityTemplate(self, entity_template_public_id: str) -> EntityTemplate:
+        """Get Entity Template returns information about a given entity template."""
+        return EntityTemplate(
+            self.get("entity-templates/" + entity_template_public_id).json()
+        )
+
+    def updateEntityTemplate(
+        self, entity_template_public_id: str, entityTemplate: EntityTemplateUpdate
+    ) -> EntityTemplate:
+        """Update an entity template’s name or its contents."""
+        return EntityTemplate(
+            self.put(
+                "entity-templates/" + entity_template_public_id, entityTemplate
+            ).json()
+        )
+
+    def deleteEntityTemplate(self, entity_template_public_id: str) -> requests.Response:
+        """Delete an entity template."""
+        return self.delete("entity-templates/" + entity_template_public_id)
+
+
+if __name__ == "__main__":
+    client = ClubhouseClient(os.environ.get("CLUBHOUSE_API_TOKEN"), debug=True)
+
+    # Categories
+    # print([x for x in client.listCategories()])
+    # print(
+    #     client.createCategory(CategoryCreate(name="foof", type=CategoryType.MILESTONE))
+    # )
+    # print(client.getCategory(36))
+    # print(client.deleteCategory(33))
+    # print(client.updateCategory(38, CategoryUpdate(color="#0000ff")))
+    # print([x for x in client.listCategoryMilestones(36)])
+
+    # Entity-Templates
+    # print(client.enableStoryTemplates())
+    # print([x for x in client.listEntityTemplates()])
+    # print(
+    #     client.createEntityTemplate(
+    #         EntityTemplateCreate(
+    #             name="deez", story_contents=StoryContentsCreate(description="test")
+    #         )
+    #     )
+    # )
+    # print(client.getEntityTemplate("5e05cf80-bb9f-4acd-95e8-18a695d59bca"))
+    # print(
+    #     client.updateEntityTemplate(
+    #         "5e05d5d6-ed99-41e0-a0c3-a8f50490034d",
+    #         EntityTemplateUpdate(
+    #             name="meese", story_contents=StoryContentsCreate(description="lul")
+    #         ),
+    #     )
+    # )
+    # print(client.deleteEntityTemplate("5e05cf80-bb9f-4acd-95e8-18a695d59bca"))
+    # print(client.disableStoryTemplates())
---
+++ testtype.py
@@ -0,0 +1,222 @@
+from enum import Enum
+from typing import TypedDict, List
+import datetime
+
+
+class Entity(TypedDict, total=False):
+    id: int
+    entity_type: str
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+
+
+##############
+# Categories #
+##############
+class CategoryType(str, Enum):
+    MILESTONE = "milestone"
+
+class Category(Entity):
+    """A Category can be used to associate Milestones."""
+    archived: bool
+    color: str
+    external_id: str
+    name: str
+    type: CategoryType
+
+class CategoryCreateRequired(TypedDict):
+    name: str
+    type: CategoryType
+
+class CategoryCreate(CategoryCreateRequired, total=False):
+    color: str
+    external_id: str
+
+class CategoryUpdate(TypedDict, total=False):
+    archived: bool
+    color: str
+    name: str
+
+#########
+# Files #
+#########
+class File(Entity):
+    """A File is any document uploaded to your Clubhouse. Files attached from a third-party service can be accessed using the Linked Files endpoint."""
+    content_type: str
+    description: str
+    external_id: str
+    filename: str
+    group_mention_ids: List[str]
+    member_mention_ids: List[str]
+    mention_ids: List[str]
+    name: str
+    size: int
+    story_ids: List[int]
+    thumbnail_url: str
+    uploader_id: str
+    url: str
+
+
+##########
+# Labels #
+##########
+class LabelStats(TypedDict, total=False):
+    """A group of calculated values for this Label."""
+    num_epics: int
+    num_points_completed: int
+    num_points_in_progress: int
+    num_points_total: int
+    num_stories_completed: int
+    num_stories_in_progress: int
+    num_stories_total: int
+    num_stories_unestimated: int
+
+class Label(Entity):
+    """A Label can be used to associate and filter Stories and Epics, and also create new Workspaces."""
+    archived: bool
+    color: str
+    description: str
+    external_id: str
+    name: str
+    stats: LabelStats
+
+class LabelCreate(Entity):
+    color: str
+    description: str
+    external_id: str
+    name: str
+
+
+################
+# Linked-Files #
+################
+class LinkedFile(Entity):
+    """Linked files are stored on a third-party website and linked to one or more Stories. Clubhouse currently supports linking files from Google Drive, Dropbox, Box, and by URL."""
+    content_type: str
+    description: str
+    group_mention_ids: List[str]
+    member_mention_ids: List[str]
+    mention_ids: List[str]
+    name: str
+    size: int
+    story_ids: List[int]
+    thumbnail_url: str
+    type: str
+    uploader_id: str
+    url: str
+
+
+###########
+# Stories #
+###########
+class ExternalTicket(TypedDict, total=False):
+    """A External Ticket allows you to create a link between an external system, like Zendesk, and a Clubhouse story."""
+    epic_ids: List[int]
+    external_id: str
+    external_url: str
+    id: str
+    story_ids: List[int]
+
+class ExternalTicketCreate(TypedDict, total=False):
+    external_id: str
+    external_url: str
+
+class StoryContentsTasks(TypedDict, total=False):
+    """A task connected to a story."""
+    complete: bool
+    description: str
+    external_id: str
+    owner_ids: List[str]
+    position: int
+
+class EntityTemplateTask(TypedDict, total=False):
+    """Request parameters for specifying how to pre-populate a task through a template."""
+    complete: bool
+    description: str
+    external_id: str
+    owner_ids: List[str]
+
+class StoryContents(TypedDict, total=False):
+    """A container entity for the attributes this template should populate."""
+    deadline: datetime.datetime
+    description: str
+    entity_type: str
+    epic_id: int
+    estimate: int
+    external_tickets: List[ExternalTicket]
+    files: List[File]
+    follower_ids: List[str]
+    iteration_id: int
+    labels: List[Label]
+    linked_files: List[LinkedFile]
+    name: str
+    owner_ids: List[str]
+    project_id: int
+    story_type: str
+    tasks: List[StoryContentsTasks]
+    workflow_state_id: int
+
+class StoryContentsCreate(TypedDict, total=False):
+    deadline: datetime.datetime
+    description: str
+    entity_type: str
+    epic_id: int
+    estimate: int
+    external_tickets: List[ExternalTicketCreate]
+    files: List[File]
+    follower_ids: List[str]
+    iteration_id: int
+    labels: List[LabelCreate]
+    linked_files: List[LinkedFile]
+    name: str
+    owner_ids: List[str]
+    project_id: int
+    story_type: str
+    tasks: List[EntityTemplateTask]
+    workflow_state_id: int
+
+
+####################
+# Entity-Templates #
+####################
+class EntityTemplate(Entity):
+    """An entity template can be used to prefill various fields when creating new stories."""
+    author_id: str
+    last_used_at: datetime.datetime
+    name: str
+    story_contents: StoryContents
+
+class EntityTemplateCreateRequired(Entity):
+    name: str
+    story_contents: StoryContentsCreate
+
+class EntityTemplateCreate(EntityTemplateCreateRequired, total=False):
+    author_id: str
+
+class EntityTemplateUpdate(TypedDict, total=False):
+    name: str
+    story_contents: StoryContentsCreate
+
+
+##############
+# Milestones #
+##############
+class MilestoneStats(TypedDict, total=False):
+    """A group of calculated values for this Milestone."""
+    average_cycle_time: int
+    average_lead_time: int
+
+class Milestone(Entity):
+    """A Milestone is a collection of Epics that represent a release or some other large initiative that your organization is working on."""
+    categories: List[Category]
+    completed: bool
+    completed_at: datetime.datetime
+    completed_at_override: datetime.datetime
+    description: str
+    name: str
+    position: int
+    started: bool
+    started_at: datetime.datetime
+    started_at_override: datetime.datetime
+    state: str
+    stats: MilestoneStats
